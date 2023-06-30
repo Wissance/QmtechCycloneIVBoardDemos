ab6783def6fbcda0a4e9ee6827b5739a274238d1
Ushakov Michael <um.nix.user@gmail.com>
2023-06-30 18:49:58 +0500

    serial echo tb with emulate data receive

--- SerialPortEcho/serial_echo_tb.v
+++ SerialPortEcho/serial_echo_tb.v
@@ -26,12 +26,103 @@ wire tx;
 reg rts;
 wire cts;
 
+reg [31:0] counter;
+
+localparam reg[31:0] RS232_BIT_TICKS = 50000000 / 115200; // == 434
+
+serial_echo tty (.clk(clk), .rst(rst), .rx(rx), .tx(tx), .rts(rts), .cts(cts));
+
 initial
 begin
+    clk <= 0;
+    counter <= 0;
+    rst <= 0;
+    rx <= 1;
+    rts <= 0;
+    #200
+    rst <= 1;
+    #200
+    rst <= 0;
 end
 
 always
 begin
+    #10 clk <= ~clk; // 50 MHz
+    counter <= counter + 1;
+    // 1. RX (reading byte without an error)
+    // 1.1 Sending Start bit
+    if (counter == 100)
+    begin
+        rx <= 1'b0;
+    end
+    // 1.2 Sending Data bits 8'b01010011
+    // b0
+    if (counter == 2 * RS232_BIT_TICKS + 100)  // we multiply on 2 because counter changes twice a period
+    begin
+       rx <= 1'b1;
+    end
+    // b1
+    if (counter == 2 * 2 * RS232_BIT_TICKS + 100)
+    begin
+       rx <= 1'b1;
+    end
+    // b2
+    if (counter == 2 * 3 * RS232_BIT_TICKS + 100)
+    begin
+       rx <= 1'b0;
+    end
+    // b3
+    if (counter == 2 * 4 * RS232_BIT_TICKS + 100)
+    begin
+       rx <= 1'b0;
+    end
+    // b4
+    if (counter == 2 * 5 * RS232_BIT_TICKS + 100)
+    begin
+       rx <= 1'b1;
+    end
+    // b5
+    if (counter == 2 * 6 * RS232_BIT_TICKS + 100)
+    begin
+       rx <= 1'b0;
+    end
+    // b6
+    if (counter == 2 * 7 * RS232_BIT_TICKS + 100)
+    begin
+       rx <= 1'b1;
+    end
+    // b7
+    if (counter == 2 * 8 * RS232_BIT_TICKS + 100)
+    begin
+       rx <= 1'b0;
+    end
+    // 1.3 Sending Parity (even)
+    if (counter == 2 * 9 * RS232_BIT_TICKS + 100)
+    begin
+       rx <= 1'b0;
+    end
+    // 1.4 Sending Stop bit
+    if (counter == 2 * 10 * RS232_BIT_TICKS + 100)
+    begin
+       rx <= 1'b1;
+    end
+    // 1.5 ASSERT on first byte
+    if (counter > 2 * 8 * RS232_BIT_TICKS + 100 && counter < 2 * 10 * RS232_BIT_TICKS + 100)
+    begin
+        `ASSERT(rx_err, 1'b0)
+    end
+    if (counter == 2 * 10 * RS232_BIT_TICKS + 200)
+    begin
+        rx_read <= 1;
+    end
+    if (counter == 2 * 10 * RS232_BIT_TICKS + 200 + 2)
+    begin
+        `ASSERT(rx_data, 8'b01010011)
+    end
+    if (counter == 2 * 10 * RS232_BIT_TICKS + 300)
+    begin
+        rx_read <= 0;
+    end
 end
 
 endmodule
