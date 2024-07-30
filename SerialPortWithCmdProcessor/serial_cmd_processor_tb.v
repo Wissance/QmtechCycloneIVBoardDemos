6e2a09526c966b9353e80ce71979486848fca4a3
Ushakov Michale <um.nix.user@gmail.com>
2024-07-30 23:15:28 +0500

    starting to do something with tb

--- SerialPortWithCmdProcessor/serial_cmd_processor_tb.v
+++ SerialPortWithCmdProcessor/serial_cmd_processor_tb.v
@@ -25,7 +25,29 @@ reg  rx;
 wire tx;
 reg  rts;
 wire cts;
+reg  [31:0] counter;
 
 serial_cmd_processor tty0(.clk(clk), .rx(rx), .tx(tx), .rts(rts), .cts(cts));
+
+initial
+begin
+    counter <= 0;
+    rx <= 1'b1;
+    clk <= 1'b0;
+end
+
+always
+begin
+    #10 clk <= ~clk; // 50 MHz
+    counter <= counter + 1;
+    // 1. Send command 0xFF 0xFF 0x00 0x02 0x02 0x03 0xEE 0xEE (Read Reg 3)
+    // 1.1 First  SOF byte - 0xFF
+    // 1.2 Second SOF byte - 0xFF
+    // 1.3 Space byte - 0x00
+    // 1.4 Payload len byte - 0x02
+    // 1.5 Payload bytes - 0x02 0x03
+    // 1.6 First  EOF byte - 0xEE
+    // 1.6 Second EOF byte - 0xEE
+end
 
 endmodule
